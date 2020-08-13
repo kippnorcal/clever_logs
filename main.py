77045a8e549a546fa8ad9914a4649f9bf6327aa1
Susan Xiong <susanxiong@rso-susan-xiong.lan>
2020-08-12 17:58:07 -0700

    PROJ21-742 Add student user/email table exports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 from datetime import datetime as dt
-import glob
+from fnmatch import fnmatch
 import logging
 import json
 import os
@@ -87,13 +87,24 @@ def get_data_from_csv(sql, data_dir):
     # the filename sometimes gets cut off due to a Clever bug,
     # so we're not searching for it by the .csv extension
     for filename in os.listdir(data_dir):
-        if filename != ".gitkeep":
+        if fnmatch(filename, f"*logins*"):
             file_path = f"{data_dir}/{filename}"
             break
     df = pd.read_csv(file_path)
     df = parse_email(df)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     df = df.astype({"timestamp": "datetime64[ns]"})
+    logging.info(f"Loaded {len(df)} records from downloaded file.")
+    return df
+
+
+def get_data_from_csv_by_name(data_dir, string_to_match):
+    """Get the downloaded csv BY NAME and store it in a dataframe."""
+    for filename in os.listdir(data_dir):
+        if fnmatch(filename, f"*{string_to_match}*"):
+            file_path = f"{data_dir}/{filename}"
+            break
+    df = pd.read_csv(file_path)
     logging.info(f"Loaded {len(df)} records from downloaded file.")
     return df
 
@@ -113,20 +124,73 @@ def close(driver):
     driver.close()
 
 
-def main():
-    # Download data from Clever
-    try:
-        data_dir = os.path.join(os.getcwd(), "data")
-        driver = create_driver(data_dir)
-        driver.implicitly_wait(5)
-        login(driver)
-        export_login_logs(driver)
-    finally:
-        close(driver)
-    # Transform and load csv data into database table
+def get_logs(data_dir, driver):
+    """Download logs data from Clever"""
+    export_login_logs(driver)
     sql = MSSQL()
+    # Transform and load csv data into database table
     df = get_data_from_csv(sql, data_dir)
     load_newest_data(sql, df)
+
+
+def export_student_users(driver):
+    """Download csv of student users"""
+    driver.get("https://schools.clever.com/browser")
+    time.sleep(10)
+    # add student filter (doesn't work if you navigate directly there)
+    driver.get(
+        "https://schools.clever.com/browser#{%22sidebar%22:%22students%22,%22filters%22:[]}"
+    )
+    time.sleep(15)
+    download_button = driver.find_element_by_id("download-all")
+    download_button.click()
+    time.sleep(10)
+    logging.info("Successfully downloaded student users.")
+
+
+def get_student_user_table(data_dir, driver):
+    """Get student users from Clever Data Browser."""
+    export_student_users(driver)
+    # Transform and load csv data into database table
+    df = get_data_from_csv_by_name(data_dir, "students")
+    MSSQL().insert_into("Clever_StudentUsers", df, if_exists="replace")
+    logging.info(f"Inserted {len(df)} new records into Clever_StudentUsers.")
+
+
+def export_student_google_accounts(driver):
+    """Download student google accounts csv"""
+    driver.get(
+        "https://schools.clever.com/school/applications/50ca15a93bc2733956000007/settings"
+    )
+    time.sleep(5)
+    iframe = driver.find_element_by_name("canvas_iframe")
+    driver.switch_to.frame(iframe)
+    time.sleep(5)
+    export_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Student Export"))
+    )
+    export_button.click()
+    logging.info("Successfully downloaded student google accounts.")
+
+
+def get_student_google_accounts(data_dir, driver):
+    """Get student emails from Google Accounts Manager app."""
+    export_student_google_accounts(driver)
+    # Transform and load csv data into database table
+    df = get_data_from_csv_by_name(data_dir, "Student_export")
+    df.rename(columns={"ID": "SIS_ID"}, inplace=True)
+    MSSQL().insert_into("Clever_StudentGoogleAccounts", df, if_exists="replace")
+    logging.info(f"Inserted {len(df)} new records into Clever_StudentGoogleAccounts.")
+
+
+def main():
+    data_dir = os.path.join(os.getcwd(), "data")
+    driver = create_driver(data_dir)
+    driver.implicitly_wait(5)
+    login(driver)
+    # get_logs(data_dir, driver)
+    get_student_user_table(data_dir, driver)
+    # get_student_google_accounts(data_dir, driver)
 
 
 if __name__ == "__main__":
