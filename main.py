9877b70ac1177e05d9b6b91a60a1dd0358954bc8
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2020-12-02 10:15:41 -0800

    PROJ21-1359 Check file is downloaded before proceeding

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 from datetime import datetime as dt
 from fnmatch import fnmatch
+import glob
 import logging
 import json
 import os
@@ -16,6 +17,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from tenacity import retry, stop_after_attempt, wait_exponential, Retrying
 
 from mailer import Mailer
 
@@ -30,15 +32,17 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %I:%M:%S%p %Z",
 )
 
+DATA_DIR = os.path.join(os.getcwd(), "data")
 
-def create_driver(data_dir):
+
+def create_driver():
     """Create Chrome webdriver with the specified preferences for automatic downloading."""
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")  # chrome crashes without this arg
     options.add_argument("--headless")
     options.add_argument("--browser.download.folderList=2")
     options.add_argument("--browser.helperApps.neverAsk.saveToDisk=text/csv")
-    prefs = {"download.default_directory": data_dir}
+    prefs = {"download.default_directory": DATA_DIR}
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
@@ -66,6 +70,14 @@ def login(driver):
     logging.info("Successfully logged in.")
 
 
+@retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=4, max=10))
+def check_for_file_download(file_string):
+    """Check if a file is downloaded based on a partial string match"""
+    files = glob.glob(f"{DATA_DIR}/*{file_string}*")
+    if len(files) == 0:
+        raise Exception(f"'{file_string}' file not found.")
+
+
 def export_login_logs(driver):
     """Download Login Logs csv"""
     driver.get("https://schools.clever.com/instant-login/logs")
@@ -74,7 +86,8 @@ def export_login_logs(driver):
         EC.presence_of_element_located((By.XPATH, '//a[@aria-label="Export as .csv"]'))
     )
     export_button.click()
-    time.sleep(15)
+    check_for_file_download("logins")
+    time.sleep(5)
     logging.info("Successfully downloaded login logs.")
 
 
@@ -87,13 +100,13 @@ def parse_email(df):
     return df
 
 
-def get_data_from_csv(sql, data_dir):
+def get_data_from_csv(sql):
     """Get the downloaded csv and store it in a dataframe."""
     # the filename sometimes gets cut off due to a Clever bug,
     # so we're not searching for it by the .csv extension
-    for filename in os.listdir(data_dir):
+    for filename in os.listdir(DATA_DIR):
         if fnmatch(filename, f"*logins*"):
-            file_path = f"{data_dir}/{filename}"
+            file_path = f"{DATA_DIR}/{filename}"
             break
     df = pd.read_csv(file_path)
     df = parse_email(df)
@@ -103,11 +116,11 @@ def get_data_from_csv(sql, data_dir):
     return df
 
 
-def get_data_from_csv_by_name(data_dir, string_to_match):
+def get_data_from_csv_by_name(string_to_match):
     """Get the downloaded csv BY NAME and store it in a dataframe."""
-    for filename in os.listdir(data_dir):
+    for filename in os.listdir(DATA_DIR):
         if fnmatch(filename, f"*{string_to_match}*"):
-            file_path = f"{data_dir}/{filename}"
+            file_path = f"{DATA_DIR}/{filename}"
             break
     df = pd.read_csv(file_path)
     logging.info(f"Loaded {len(df)} records from downloaded file.")
@@ -129,11 +142,11 @@ def close(driver):
     driver.close()
 
 
-def get_logs(sql, data_dir, driver):
+def get_logs(sql, driver):
     """Download logs data from Clever"""
     export_login_logs(driver)
     # Transform and load csv data into database table
-    df = get_data_from_csv(sql, data_dir)
+    df = get_data_from_csv(sql)
     load_newest_data(sql, df)
 
 
@@ -148,15 +161,16 @@ def export_student_users(driver):
     time.sleep(15)
     download_button = driver.find_element_by_id("download-all")
     download_button.click()
-    time.sleep(10)
+    check_for_file_download("students")
+    time.sleep(5)
     logging.info("Successfully downloaded student users.")
 
 
-def get_student_user_table(sql, data_dir, driver):
+def get_student_user_table(sql, driver):
     """Get student users from Clever Data Browser."""
     export_student_users(driver)
     # Transform and load csv data into database table
-    df = get_data_from_csv_by_name(data_dir, "students")
+    df = get_data_from_csv_by_name("students")
     sql.insert_into("Clever_StudentUsers", df, if_exists="replace")
     logging.info(f"Inserted {len(df)} new records into Clever_StudentUsers.")
 
@@ -174,29 +188,29 @@ def export_student_google_accounts(driver):
         EC.presence_of_element_located((By.LINK_TEXT, "Student Export"))
     )
     export_button.click()
-    time.sleep(10)
+    check_for_file_download("Student_export")
+    time.sleep(5)
     logging.info("Successfully downloaded student google accounts.")
 
 
-def get_student_google_accounts(sql, data_dir, driver):
+def get_student_google_accounts(sql, driver):
     """Get student emails from Google Accounts Manager app."""
     export_student_google_accounts(driver)
     # Transform and load csv data into database table
-    df = get_data_from_csv_by_name(data_dir, "Student_export")
+    df = get_data_from_csv_by_name("Student_export")
     df.rename(columns={"ID": "SIS_ID"}, inplace=True)
     sql.insert_into("Clever_StudentGoogleAccounts", df, if_exists="replace")
     logging.info(f"Inserted {len(df)} new records into Clever_StudentGoogleAccounts.")
 
 
 def main():
-    data_dir = os.path.join(os.getcwd(), "data")
-    driver = create_driver(data_dir)
+    driver = create_driver()
     driver.implicitly_wait(5)
     login(driver)
     sql = MSSQL()
-    get_logs(sql, data_dir, driver)
-    get_student_user_table(sql, data_dir, driver)
-    get_student_google_accounts(sql, data_dir, driver)
+    get_logs(sql, driver)
+    get_student_user_table(sql, driver)
+    get_student_google_accounts(sql, driver)
 
 
 if __name__ == "__main__":
